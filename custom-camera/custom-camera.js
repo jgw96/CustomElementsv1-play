c26919e9775ef671a93e134fed31340c18f10503
Justin Willis <jgw9617@gmail.com>
2016-10-27 18:51:34 -0500

    updates

--- custom-camera/custom-camera.js
+++ custom-camera/custom-camera.js
@@ -27,29 +27,69 @@ customElements.define('custom-camera', class extends HTMLElement {
   }
 
   snapShot() {
-    const canvas = this.parentNode.querySelector('canvas');
-    const context = canvas.getContext('2d');
-    const video = this.parentNode.querySelector('video');
+    let clickAnimation = this.animate(
+      [
+        { transform: 'translateY(0)' },
+        { transform: 'translateY(15%)'},
+        { transform: 'translateY(0)' }
+      ], {
+        easing: 'ease-in-out',
+        duration: 500
+      }
+    );
+    clickAnimation.onfinish = () => {
+      if (this.parentNode.querySelector('li') !== null) {
+        let elements = this.parentNode.querySelectorAll('li');
+        elements.forEach((item) => {
+          let animation = item.animate([
+            { transform: 'translateX(110%)' }
+          ], {
+            duration: 700,
+            easing: 'ease-out'
+          });
+          animation.onfinish = () => {
+            item.parentNode.removeChild(item);
+          };
+        });
+      }
+      const canvas = this.parentNode.querySelector('canvas');
+      const context = canvas.getContext('2d');
+      const video = this.parentNode.querySelector('video');
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    console.log(canvas.toDataURL('image/jpg'));
-    const imageData = canvas.toDataURL('image/jpg');
-    fetch('http://localhost:8080/label', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        data: imageData
+      const imageData = canvas.toDataURL('image/jpg');
+      fetch('http://localhost:8080/label', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          data: imageData
+        })
       })
-    })
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(err => console.error(err));
-  }
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data.data);
+        const list = this.parentNode.querySelector('ul');
+        data.data.forEach((label) => {
+          let listItem = document.createElement('li');
+          let node = document.createTextNode(label);
+          listItem.appendChild(node);
+
+          list.appendChild(listItem);
+          listItem.animate([
+            { opacity: 0 },
+            { opacity: 1 }
+          ], {
+            easing: 'ease-in',
+            duration: 500
+          });
+        });
+      })
+      .catch(err => console.error(err));
+      }
+    }
 });
